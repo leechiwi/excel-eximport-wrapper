5cbce490ccc0bd77fb9643723950ba834743e1ec
lijiwei <lijiwei@thunisoft.com>
2023-03-14 15:38:29 +0800

    branch develop updated back,because code shoule be writed in branch master

--- src/main/java/com/leechiwi/excel/util/Zip.java
+++ src/main/java/com/leechiwi/excel/util/Zip.java
@@ -19,7 +19,7 @@ public class Zip {
             fileNames = new ArrayList<>();
             for(int i=0,len=streamList.size();i<len;i++){
                 StringBuilder sb=new StringBuilder();
-                fileNames.add(sb.append(i).toString());
+                fileNames.add(sb.append(i).append(ext).toString());
             }
         }
         if(streamList.size()!=fileNames.size()){
@@ -30,12 +30,11 @@ public class Zip {
         try {
             for (byte[] bytes : streamList) {
                 // 创建一个ZipEntry
-                zipOut.putNextEntry(new ZipEntry(fileNames.get(index)+ext));
+                zipOut.putNextEntry(new ZipEntry(fileNames.get(index)+".xlsx"));
                 // 将源文件的字节内容，写入zip压缩包
                 zipOut.write(bytes);
                 // 结束当前zipEntry
                 zipOut.closeEntry();
-                index++;
             }
             zipOut.finish();
             zipOut.close();
