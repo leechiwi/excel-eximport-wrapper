42b0610dc895b57424b908bf8d543bbe0861e4ca
lijiwei <lijiwei@thunisoft.com>
2023-03-21 16:41:39 +0800

    if  the property of a class  has @ColumnWidth then get its value as used width

--- src/main/java/com/leechiwi/excel/handler/DefaultWidthStyleStrategy.java
+++ src/main/java/com/leechiwi/excel/handler/DefaultWidthStyleStrategy.java
@@ -4,6 +4,7 @@ import com.alibaba.excel.enums.CellDataTypeEnum;
 import com.alibaba.excel.metadata.Head;
 import com.alibaba.excel.metadata.data.CellData;
 import com.alibaba.excel.metadata.data.WriteCellData;
+import com.alibaba.excel.metadata.property.ColumnWidthProperty;
 import com.alibaba.excel.write.metadata.holder.WriteSheetHolder;
 import com.alibaba.excel.write.style.column.AbstractColumnWidthStyleStrategy;
 import org.apache.commons.collections.CollectionUtils;
@@ -11,15 +12,17 @@ import org.apache.poi.ss.usermodel.Cell;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class DefaultWidthStyleStrategy extends AbstractColumnWidthStyleStrategy {
     private static final int MAX_COLUMN_WIDTH = 50;
+    private List <Integer> defaultHeadColumnWidths=new ArrayList<>();//此处代表各表头列对应注解@ColumnWidth宽度
     private List <Integer> headColumnWidths=new ArrayList<>();//此处代表各表头列对应宽度
     @Override
     protected void setColumnWidth(WriteSheetHolder writeSheetHolder, List<WriteCellData<?>> cellDataList, Cell cell, Head head, Integer relativeRowIndex, Boolean isHead) {
         boolean needSetWidth = isHead || !CollectionUtils.isEmpty(cellDataList);
         if (needSetWidth) {
-            Integer columnWidth = this.dataLength(cellDataList, cell, isHead);
+            Integer columnWidth = this.dataLength(cellDataList, cell, isHead,head);
             if (columnWidth >= 0) {
                 if (columnWidth > MAX_COLUMN_WIDTH) {
                     columnWidth = MAX_COLUMN_WIDTH;
@@ -28,12 +31,17 @@ public class DefaultWidthStyleStrategy extends AbstractColumnWidthStyleStrategy 
                 if (columnWidth < currentHeadWidth) {//内容长度小于对应表头列的长度则以表头列长度为准，否则表头别里面内容会折行
                     columnWidth = currentHeadWidth;
                 }
+                if(this.defaultHeadColumnWidths.get(cell.getColumnIndex())>0){
+                    columnWidth=this.defaultHeadColumnWidths.get(cell.getColumnIndex());
+                }
                 writeSheetHolder.getSheet().setColumnWidth(cell.getColumnIndex(), columnWidth * 256);
             }
         }
     }
-    private Integer dataLength(List<?> cellDataList, Cell cell, Boolean isHead) {
+    private Integer dataLength(List<?> cellDataList, Cell cell, Boolean isHead, Head head) {
         if (isHead) {
+            Integer colWidth = Optional.ofNullable(head.getColumnWidthProperty()).map(ColumnWidthProperty::getWidth).orElse(0);
+            this.defaultHeadColumnWidths.add(colWidth);
             int length = cell.getStringCellValue().getBytes().length;
             if (this.headColumnWidths.size()>cell.getColumnIndex()) {//获取表头每列的最长宽度（保证导出时候表头每一列都展示完整且不折行）
                 Integer currentHeadWidth =this.headColumnWidths.get(cell.getColumnIndex());
